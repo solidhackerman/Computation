3b4dae62e1cda3b32c9ac13b63358ef077ecff4a
solidhackerman <vihaansharma@Vihaans-MacBook-Air.local>
2019-03-12 11:32:15 +0530

    v2.0

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,6 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numberOne: UITextField!
+    @IBOutlet weak var numberTwo: UITextField!
+    
+    @IBOutlet weak var resultAns: UILabel!
+    @IBAction func buttonOp(_ sender: UIButton) {
+        if let a = Double(numberOne.text!){
+            if let b = Double(numberTwo.text!){
+                var result = 0.00
+                switch sender.titleLabel!.text{
+                case "➕":
+                    result = a+b
+                case "➖":
+                    result = a-b
+                case "✖️":
+                    result = a*b
+                case "➗":
+                    result = a/b
+                default:
+                    break
+                }
+                resultAns.text = String(result)
+                numberOne.text = resultAns.text
+                numberTwo.text = ""
+            }
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
